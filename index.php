926ad15d65854d6ac080c68970dfcdf646879351
MarcoB <mrc.bsllt@gmail.com>
2021-01-20 14:46:07 +0100

    completato snack 1

--- index.php
+++ index.php
@@ -5,14 +5,27 @@
       "home_team" => ["team" => "Hawks", "points_made" => 10],
       "visiting_team" => ["team" => "Nets", "points_made" => 6]
     ],
+    [
+      "home_team" => ["team" => "Celtics", "points_made" => 15],
+      "visiting_team" => ["team" => "Bulls", "points_made" => 20]
+    ],
+    [
+      "home_team" => ["team" => "Pacers", "points_made" => 32],
+      "visiting_team" => ["team" => "Knicks", "points_made" => 11]
+    ],
+    [
+      "home_team" => ["team" => "Heat", "points_made" => 40],
+      "visiting_team" => ["team" => "Hornets", "points_made" => 32]
+    ],
+    [
+      "home_team" => ["team" => "Pistons", "points_made" => 32],
+      "visiting_team" => ["team" => "Raptors", "points_made" => 18]
+    ],
+    [
+      "home_team" => ["team" => "Wizards", "points_made" => 5],
+      "visiting_team" => ["team" => "Magic", "points_made" => 21]
+    ],
   ];
-
-  $homeTeam = $matches[0]["home_team"]["team"];
-  $homePoints = $matches[0]["home_team"]["points_made"];
-  $visitingTeam = $matches[0]["visiting_team"]["team"];
-  $visitingPoints = $matches[0]["visiting_team"]["points_made"];
-
-
 
 ?>
 
@@ -20,9 +33,21 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style1.css">
     <title>php blocco 1</title>
   </head>
   <body>
-    <p><?php echo $homeTeam."-".$visitingTeam." | ".$homePoints."-".$visitingPoints; ?></p>
+    <ul>
+      <?php
+        for($i = 0; $i < count($matches); $i++) {
+          $homeTeam = $matches[$i]["home_team"]["team"];
+          $homePoints = $matches[$i]["home_team"]["points_made"];
+          $visitingTeam = $matches[$i]["visiting_team"]["team"];
+          $visitingPoints = $matches[$i]["visiting_team"]["points_made"];
+
+          echo "<li>".$homeTeam." - ".$visitingTeam." | ".$homePoints." - ".$visitingPoints."</li>";
+        }
+      ?>
+  </ul>
   </body>
 </html>
