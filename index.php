42457627ee7be746f50d7c9c17b7c946a479c882
MarcoB <mrc.bsllt@gmail.com>
2021-01-20 14:17:48 +0100

    creato struttura array

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-  
+
+  $matches = [
+    [
+      "home_team" => ["team" => "Hawks", "points_made" => 10],
+      "visiting_team" => ["team" => "Nets", "points_made" => 6]
+    ],
+  ];
+
+  $homeTeam = $matches[0]["home_team"]["team"];
+  $homePoints = $matches[0]["home_team"]["points_made"];
+  $visitingTeam = $matches[0]["visiting_team"]["team"];
+  $visitingPoints = $matches[0]["visiting_team"]["points_made"];
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,5 +23,6 @@
     <title>php blocco 1</title>
   </head>
   <body>
+    <p><?php echo $homeTeam."-".$visitingTeam." | ".$homePoints."-".$visitingPoints; ?></p>
   </body>
 </html>
